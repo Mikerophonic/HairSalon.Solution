03fba47a4df3f80f7ffc4274496fc8c2c5566bee
Mikerophonic <micgcon@gmail.com>
2023-10-05 08:39:41 -0700

    make clients create view

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -3,3 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    public ActionResult Create()
+    {
+        SelectList selectList = new SelectList(_db.Stylists, "StylistId", "Name");
+        ViewBag.StylistId = selectList;
+        return View();
+    }
+  }
+}
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,5 +4,6 @@ namespace HairSalon.Models
   {
     public int StylistId { get; set; }
     public string Name { get; set; }
+    public List<Client> Clients { get; set; }
   }
 }
